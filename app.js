eb37fd74f4d1371818db6f97f94ca4adbdd1ebf7
Js-28 <shahjainish38837@gmail.com>
2022-10-05 02:25:12 +0530

    OAUTH with google sign in and authentication of level 6 is done

--- app.js
+++ app.js
@@ -15,12 +15,18 @@ const passportLocalMongoose=require("passport-local-mongoose");
 //const _=require("lodash"); //for captialize the string
 // const date = require(__dirname + "/date.js");
 
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
+
 const app = express();
 
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+
 
 app.use(session({
   secret: "Our little secret.",
@@ -35,26 +41,71 @@ app.use(passport.session());
 
 
 mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser:true});
-app.get("/",function(req,res){
-  res.render("home");
-});
+
 
 const userSchema=new mongoose.Schema({
   email:String,
-  password:String
+  password:String,
+  googleId:String
 });
 
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 //userSchema.plugin(encrypt, { secret:process.env.SECRET,encryptedFields: ["password"] }); // For  encryption 
 
-const User =mongoose.model("User",userSchema);
+const User =new mongoose.model("User",userSchema);
 
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+//works just for local strategy as used passport-local-mongoose here.
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+//Valid for strategy i.e. google,local etc used here as we use this from passport js.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+//it is put here because after intializing and after creating a session we need of this redirection so...
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+//to redirect to auth page as it was been transfered as we pressed google button declared in register.ejs and login.ejs
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] }));  //Will use google strategy declared above and will give profile info that contains email and user id.
+
+//to redirect the url i.e declared auth/google/secrets on developers engine after successfully authenticating and storing the cookies and session.
+//And will locally authenticate user by session and cookies.
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }), //If any failures occurs then will be redirected to login page.
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect("/secrets");
+  });
 
 
 app.get("/login",function(req,res){
